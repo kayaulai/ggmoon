88ca3336e897dee35e4052002c1769d9144a5bb7
Ryan Ka Yau Lai <43101723+kayaulai@users.noreply.github.com>
2023-07-16 03:53:55 -0700

    Add classic correspondence analysis feature

--- R/draw.R
+++ R/draw.R
@@ -1,3 +1,18 @@
+tilt = function(theta, flip = F){
+  if(!flip){
+    (pi/2 - theta) / pi * 180
+  } else {
+    sapply(theta, function(x){
+      if(x < pi){
+        (pi/2 - x) / pi * 180
+      } else {
+        (pi/2 - x) / pi * 180 + 180
+      }
+    })
+  }
+}
+
+
 #' @rdname getMoonplot
 #' @export
 getMoonplot = function(result, ...){
@@ -15,29 +30,44 @@ getMoonplot = function(result, ...){
 #' @export
 #'
 #' @examples
-getMoonplot.MCA = function(result, rextra = .1, textmax = .2){
-  norms_ind = sqrt((res.mca$ind$coord[,1])^2 + (res.mca$ind$coord[,2])^2)
-  theta_ind = atan(res.mca$ind$coord[,1] / res.mca$ind$coord[,2]) + (res.mca$ind$coord[,2] < 0) * pi + (res.mca$ind$coord[,2] > 0 & res.mca$ind$coord[,1] < 0) * 2 * pi
+getMoonplot.MCA = function(result, rextra = .1, textmax = .2, flip = F, row_col = NULL, col_col = NULL){
+  getMoonplot_general(result$ind$coord, result$var$coord, rextra = rextra, textmax = textmax, flip = flip, row_col = row_col, col_col = col_col)
+}
+
+#' @rdname getMoonplot
+#' @export
+getMoonplot.CA = function(result, rextra = .1, textmax = .2, flip = F, label_rows = F, row_col = NULL, col_col = NULL){
+  getMoonplot_general(result$row$coord, result$col$coord, rextra = rextra, textmax = textmax, flip = flip, label_rows = label_rows, row_col = row_col, col_col = col_col)
+}
+
+getMoonplot_general = function(row_coords, col_coords, rextra = .1, textmax = .2, flip = F, label_rows = F, row_col = NULL, col_col = NULL){
+  norms_row = sqrt((row_coords[,1])^2 + (row_coords[,2])^2)
+  theta_row = atan(row_coords[,1] / row_coords[,2]) + (row_coords[,2] < 0) * pi + (row_coords[,2] > 0 & row_coords[,1] < 0) * 2 * pi
   quadrant = c(case_when(
-    res.mca$ind$coord[, 1] > 0 & res.mca$ind$coord[, 2] > 0 ~ 1L,
-    res.mca$ind$coord[, 1] > 0 & res.mca$ind$coord[, 2] < 0 ~ 2L,
-    res.mca$ind$coord[, 1] < 0 & res.mca$ind$coord[, 2] < 0 ~ 3L,
+    row_coords[, 1] > 0 & row_coords[, 2] > 0 ~ 1L,
+    row_coords[, 1] > 0 & row_coords[, 2] < 0 ~ 2L,
+    row_coords[, 1] < 0 & row_coords[, 2] < 0 ~ 3L,
     T ~ 4L
   ))
 
-  norms_var = sqrt((res.mca$var$coord[,1])^2 + (res.mca$var$coord[,2])^2)
-  theta_var = atan(res.mca$var$coord[,1] / res.mca$var$coord[,2]) + (res.mca$var$coord[,2] < 0) * pi + (res.mca$var$coord[,2] > 0 & res.mca$var$coord[,1] < 0) * 2 * pi
+  norms_col = sqrt((col_coords[,1])^2 + (col_coords[,2])^2)
+  theta_col = atan(col_coords[,1] / col_coords[,2]) + (col_coords[,2] < 0) * pi + (col_coords[,2] > 0 & col_coords[,1] < 0) * 2 * pi
 
 
-  ind_df = data.frame(theta_ind, norms_ind)
-  var_df = data.frame(name = rownames(res.mca$var$coord), theta_var, norms_var)
+  row_df = data.frame(name = rownames(row_coords), theta_row, norms_row)
+  col_df = data.frame(name = rownames(col_coords), theta_col, norms_col)
 
-  plot_inds = ggplot(ind_df, aes(x = theta_ind, y = norms_ind)) +
-    geom_point() +
+  point_aes = aes()
+  if(!is.null(row_col)){
+    point_aes = aes(col = as.factor(row_col))
+  }
+
+  plot_rows = ggplot(row_df, aes(x = theta_row, y = norms_row)) +
+    geom_point(mapping = point_aes) +
     scale_colour_discrete() +
     scale_x_continuous(limits = c(0, 2 * pi), expand = c(0, 0)) +
-    scale_y_continuous(breaks = max(norms_ind) + rextra,
-                       limits = c(0, max(norms_ind) + rextra + textmax))+
+    scale_y_continuous(breaks = max(norms_row) + rextra,
+                       limits = c(0, max(norms_row) + rextra + textmax))+
     coord_polar(theta = "x")  +
     theme_minimal() +
     theme(axis.text.x = element_blank(),
@@ -49,9 +79,19 @@ getMoonplot.MCA = function(result, rextra = .1, textmax = .2){
           panel.grid.major = element_blank(),
           panel.grid.minor = element_blank(),
           legend.position = "none") +
-    geom_hline(yintercept = max(norms_ind) + rextra)
+    geom_hline(yintercept = max(norms_row) + rextra)
 
-  plot_full = plot_inds +
-    geom_text(data = var_df, aes(x = theta_var, alpha = norms_var,  size = norms_var, label = name, angle = (pi/2 - theta_var) / pi * 180), y = max(norms_ind) + rextra, vjust = "outward", hjust = "outward")
+  plot_full = plot_rows +
+    geom_text(data = col_df, aes(x = theta_col, alpha = norms_col,  size = norms_col, label = name, angle = tilt(theta_col, flip)), y = max(norms_row) + rextra, vjust = "outward", hjust = "outward") +
+    scale_size(guide = "none")
+
+  if(label_rows){
+    text_aes = aes(x = theta_row, y = norms_row, label = name)
+    if(!is.null(row_col)){
+      text_aes = modifyList(text_aes, aes(col = as.factor(row_col)))
+    }
+    plot_full = plot_full + ggrepel::geom_text_repel(text_aes)
+  }
   plot_full
 }
+
--- tests/testthat/test-getMoonplot.R
+++ tests/testthat/test-getMoonplot.R
@@ -1,7 +1,13 @@
-test_that("multiplication works", {
+test_that("MCA works", {
   data(tea)
   res.mca = MCA(tea, quanti.sup=19, quali.sup=20:36, graph = F)
   res.mca$ind$coord[,1:2] %>% data.frame %>% ggplot(aes(x = `Dim.1`, y = `Dim.2`)) + geom_point()
   res.mca$var$coord[,1:2] %>% data.frame %>% ggplot(aes(x = `Dim.1`, y = `Dim.2`)) + geom_point()
-  getMoonplot(res.mca)
+  getMoonplot(res.mca, flip = T, row_col = tea[,20])
+})
+
+test_that("CA works", {
+  data(children)
+  res.ca = CA(children, row.sup = 15:18, col.sup = 6:8, graph = F)
+  getMoonplot(res.ca, flip = T)
 })
